6a57b2f2d573a71d3be1eb06e970cebdec34d2bf
Ruslan Myznikov <ruslan.myznikov@nure.ua>
2019-07-25 13:13:08 +0300

    change root directory

--- testTask/src/app/store/actions/user.actions.ts
+++
@@ -1,7 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-import { User } from 'src/app/models/user.model';
-
-export const setUsers = createAction('[User List] Add', props< {users: User[]} >())
-export const editUser = createAction('[User Detail] Edit',  props< User >());
-export const createUser = createAction('[User Detail] Create',  props< User >());
-export const killUser = createAction('[User Detail] Remove',  props< User >());
